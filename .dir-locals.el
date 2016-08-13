8708581f5c45f21a76798da3b074d0dfdea6b965
Martin Törnqvist <m.tornq@gmail.com>
2016-08-13 15:16:46 +0200

    Added Emacs project configuration

---
+++ .dir-locals.el
@@ -0,0 +1,22 @@
+((nil
+  . ((eval . (progn
+               (require 'projectile)
+               (setq company-clang-arguments (delete-dups (append
+                                                           company-clang-arguments
+                                                           (list
+                                                            "-std=c++11"
+                                                            (concat "-I" (projectile-project-root) "include")
+                                                            (concat "-I" (projectile-project-root) "rl_utils/include")
+                                                            "-I/usr/include/SDL2"
+                                                            ))))))))
+ (c++-mode
+  (whitespace-line-column . 80)
+  (eval ignore-errors
+        (add-hook 'write-contents-functions
+                  (lambda ()
+                    (delete-trailing-whitespace)
+                    nil))
+        (require 'whitespace)
+        (whitespace-mode 0)
+        (whitespace-mode 1)))
+ )
